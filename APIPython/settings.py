2c964d42a55264c735b4f8154739de0a0108430d
Lili Cardoso <lili200cardoso@outlook.com.br>
2023-04-18 14:12:00 -0300

    Cria rota para AppGrades

--- APIPython/settings.py
+++ APIPython/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    #"APIPython.grades.apps.GradesConfig",
+    "APIPython.grades",
 ]
 
 MIDDLEWARE = [
